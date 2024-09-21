efa2f990070825b1d1077e9f642463120af70b39
abdushakoor12 <abdushakoor009@yahoo.com>
2024-09-21 20:39:45 +0500

    remove loon

--- lib/di.dart
+++ lib/di.dart
@@ -1,14 +1,7 @@
-import 'package:flutter/foundation.dart';
-import 'package:loon/loon.dart';
-
 final di = _DIContainer();
 
 class _DIContainer {
-  // all app dependencies that require future initialization
-  // should be initialized here
   Future<void> init() async {
-    // loon peristence
-    Loon.configure(persistor: FilePersistor(), enableLogging: kDebugMode);
-    await Loon.hydrate();
+
   }
 }
